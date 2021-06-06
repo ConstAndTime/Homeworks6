66a2c5d6b45f632aa596a600cffc7d2de6c8322d
ConstAndTime <nbfbirov@mail.ru>
2021-06-06 22:09:00 +0300

    First zadanie complited

--- hm6.cpp
+++ hm6.cpp
@@ -3,15 +3,21 @@
 
 int main(int argc, const char** argv) {
 
-  std::ofstream t1("O:/HM/hm6/text1.txt");
-  std::ofstream t2("O:/HM/hm6/text2.txt");
+  std::ofstream t1("text1.txt");
 
-  t1.open("text1.txt");
-  t1 <<  "Kabi ya bila carica, govorit odna devica";
-  t1 << "ya b dlya batushki carya, rodila b intmainirya.";
-  t1 << "Rodila carica v noch, ne to sina, todo doich.";
-  t1 << "Doich xotel bit' programmistom C++, no reshil, Pascal ostanovlus'";
-t1.close("text1.txt");
+  t1 << "Kabi ya bila carica, govorit odna devica" "\n";
+  t1 << "ya b dlya batushki carya, rodila b intmainirya." "\n";\
+  t1 << "Rodila carica v noch, ne to sina, todo doich." "\n";\
+  t1 << "Doich xotel bit' programmistom C++, no reshil, Pascal ostanovlus'" "\n";
+t1.close();
+
+std::ofstream t2("text2.txt");
+
+t2 << "Zdes' mogla bit' vasha reklama" "\n";
+t2 << "No ee zdes' ne budet..." "\n";
+t2 << "Tak kak, dlya togo, chtob chto to reklamirotvat'" "\n";
+t2 << "Nujen kontent, nujna poseshyaemost', i interesnie idei" "\n";
+t2.close();
 
   return 0;
 }
