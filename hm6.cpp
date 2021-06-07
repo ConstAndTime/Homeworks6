0da24762b3835a264fda53d0381668b63dad0cd7
ConstAndTime <nbfbirov@mail.ru>
2021-06-07 21:05:12 +0300

    Complited 2 zadanya

--- hm6.cpp
+++ hm6.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 #include<fstream>
+#include<string>
+
+void soedenylka(const std::string& from, const std::string& to) {
+std::ifstream fis(from);
+std::ofstream fos(to, std::ofstream::app);
+
+char c;
+while (fis.get(c) && !fis.eof()) {
+  fos << c;
+}
+
+fis.close();
+fos.close();
+}
+
 
 int main(int argc, const char** argv) {
 
@@ -18,6 +33,9 @@ t2 << "No ee zdes' ne budet..." "\n";
 t2 << "Tak kak, dlya togo, chtob chto to reklamirotvat'" "\n";
 t2 << "Nujen kontent, nujna poseshyaemost', i interesnie idei" "\n";
 t2.close();
+
+soedenylka("text1.txt", "text3.txt");
+soedenylka("text2.txt", "text3.txt");
 
   return 0;
 }
